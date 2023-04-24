cc04a01279c9a0ddcbd60884c41399ea14f16277
Kyle Espinola <kyle.s.espinola@gmail.com>
2023-04-23 22:29:30 -0600

    fix: codegen after mergining schema

--- codegen.ts
+++ codegen.ts
@@ -3,28 +3,35 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const holaplexSchema = {
-  [process.env.HOLAPLEX_API_ENDPOINT as string]: {
-    headers: {
-      Authorization: process.env.HOLAPLEX_AUTH_TOKEN as string,
-    },
-  },
-};
-
 const config: CodegenConfig = {
   overwrite: true,
   documents: ["**/*.graphql"],
-  schema: [holaplexSchema, "./schema.graphql"],
+  schema: [
+    {
+      [process.env.HOLAPLEX_API_ENDPOINT as string]: {
+        headers: {
+          Authorization: process.env.HOLAPLEX_AUTH_TOKEN as string,
+        },
+      },
+    },
+    "./schema.graphql",
+  ],
   generates: {
+    "./holaplex.graphql": {
+      schema: {
+        [process.env.HOLAPLEX_API_ENDPOINT as string]: {
+          headers: {
+            Authorization: process.env.HOLAPLEX_AUTH_TOKEN as string,
+          },
+        },
+      },
+      plugins: ["schema-ast"],
+    },
     "./@types/graphql.d.ts": {
       plugins: ["typescript-graphql-files-modules"],
     },
     "./src/graphql.types.ts": {
       plugins: ["typescript", "typescript-resolvers"],
-    },
-    "./holaplex.graphql": {
-      schema: holaplexSchema,
-      plugins: ["schema-ast"],
     },
   },
 };
